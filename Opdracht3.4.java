187a0e143d54b5a2d3c1bcf488fe7a67c98d1b81
Jaco van den Bosch <jacovdboschjr@gmail.com>
2016-09-12 16:33:14 +0200

    Refactor opdracht3.4

--- Opdracht3.4.java
+++ Opdracht3.4.java
@@ -2,46 +2,96 @@ import java.util.Scanner;
 
 public class Opdracht {
 
+    /**
+     * Scanner instance
+     */
     protected static Scanner reader;
-    protected static int result;
 
     public static void main(String[] args)
     {
         reader = new Scanner(System.in);
 
-        System.out.println("Wat wil je converteren: cel2kel, cel2fahr, fahr2cel");
-        String method = reader.next();
 
-        System.out.println("Hoeveel graden?");
-        int degrees = reader.nextInt();
+        String method = getMethod();
+        int degrees = getDegrees();
 
-        switch (method) {
-            case "cel2kel":
-                result = CelciusToKelvin(degrees);
-                break;
-            case "cel2fahr":
-                result = CelciusToFahrenheit(degrees);
-                break;
-            case "fahr2cel":
-                result = FahrenheitToCelcius(degrees);
-                break;
-            default:
-                result = 0;
-        }
+        int result = getResult(method, degrees);
 
         System.out.println(result);
     }
 
+    /**
+     * Get the result of the calculation
+     *
+     * @param method
+     * @param degrees
+     * @return int
+     */
+    protected static int getResult(String method, int degrees)
+    {
+        switch (method) {
+            case "cel2kel":
+                return CelciusToKelvin(degrees);
+            case "cel2fahr":
+                return CelciusToFahrenheit(degrees);
+            case "fahr2cel":
+                return FahrenheitToCelcius(degrees);
+        }
+
+        return 0;
+    }
+
+    /**
+     * Ask the user what conversion we have to do.
+     *
+     * @return String
+     */
+    protected static String getMethod()
+    {
+        System.out.println("Wat wil je converteren: cel2kel, cel2fahr, fahr2cel");
+
+        return reader.next();
+    }
+
+    /**
+     * Ask the user how many degrees he wants to convert
+     *
+     * @return int
+     */
+    protected static int getDegrees()
+    {
+        System.out.println("Hoeveel graden?");
+
+        return reader.nextInt();
+    }
+
+    /**
+     * Calculate celcius to kelvin
+     *
+     * @param degrees
+     * @return int
+     */
     protected static int CelciusToKelvin(int degrees)
     {
         return (273 + degrees);
     }
 
+    /**
+     * Calculate Celcius to Fahrenheit
+     *
+     * @param degrees
+     * @return int
+     */
     protected static int CelciusToFahrenheit(int degrees)
     {
         return (9/5 * degrees + 32);
     }
 
+    /**
+     * Calculate Fahrenheit to Ceclius
+     * @param degrees
+     * @return
+     */
     protected static int FahrenheitToCelcius(int degrees)
     {
         return 5/9 * (degrees - 32);
